b4ca4c184dea34f1117ce548f7b48a6cd069b8ec
Victor CUAU <victor.cuau@etu.univ-grenoble-alpes.fr>
2018-03-21 15:49:59 +0100

    essai1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export interface Result {
+  title: string;
+  author: string;
+  image: string;
+}
+
+const fakeResults: Result[] =[
+  {title: 'dnfkjnd1', author: ''},
+  {title: 'dnfkjnd2', author: ''}
+];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
