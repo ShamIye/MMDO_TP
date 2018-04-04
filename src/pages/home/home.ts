97f2f6aa7b3f0aa1658b7ff0eb8c06dd6453e3ec
Victor CUAU <victor.cuau@etu.univ-grenoble-alpes.fr>
2018-04-04 15:55:28 +0200

    potit chat (fin TP2)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,34 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
+import { api_key } from '../../app/tmdb';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+//import { HttpParams } from '@angular/common/http';
 
 export interface Result {
   title: string;
-  author: string;
-  //date: string;
-  image: string;
+  poster_path: string;
+  release_date: string;
+  adult: boolean;
+  original_title: string;
+  original_language: string;
+  id: number;
 }
-
-const fakeResults: Result[] =[
-  { title: 'dnfkjnd1', author: '', image:'https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/11880512_929939130419296_4742120412934412702_n.jpg?_nc_cat=0&_nc_eui2=v1%3AAeE3FZVdrHNcImMbyfTIkJ6OaEtK0WfGzAYIM2PhE-hewCYIdxt37gq-9pSikqdV8kkMbJwFIB1rAE-5fOJFtjkb2DSE_BrQg8YI3UEUp-EQnA&oh=f1fdd3fbfc696d2f159da6ccc802a07d&oe=5B69D597'},
-  {title: 'dnfkjnd2', author: '', image:''}
-];
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage{
-  results: Result[];
+  results: Observable<Result[]>;
   pushPage: typeof DetailsPage;
 
-  constructor(){
+  constructor(private http: HttpClient){
     this.pushPage = DetailsPage;
-    this.results = fakeResults;
+    this.results = Observable.of([]);
   }
   getItems(ev: any) {
     //set val to teh vamlue of the searchbar
     let val = ev.target.value;
-    return val ? this.results = fakeResults : this.results = [];
+    if(val)
+      this.results = this.fetchResults(val);
+    else
+      this.results = Observable.of([]);
+  }
+
+  fetchResults(search: string): Observable<Result[]>{
+    let url: string = 'https://api.themoviedb.org/3/search/movie'
+    return this.http.get<Result[]>(url,{
+      params :{
+        api_key: api_key,
+        query: search,
+        language : 'fr',
+      }
+    }).pluck('results');
   }
 }
