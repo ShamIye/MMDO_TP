04536f3f534616099b65744dcd90e9defd7ef107
Leya Badat <leya.badat@etu.univ-grenoble-alpes.fr>
2018-04-04 14:39:46 +0200

    dédicace2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,12 +6,12 @@ export interface Result {
   title: string;
   author: string;
   //date: string;
-  //image: string;
+  image: string;
 }
 
 const fakeResults: Result[] =[
-  {title: 'dnfkjnd1', author: ''},
-  {title: 'dnfkjnd2', author: ''}
+  { title: 'dnfkjnd1', author: '', image:'https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/11880512_929939130419296_4742120412934412702_n.jpg?_nc_cat=0&_nc_eui2=v1%3AAeE3FZVdrHNcImMbyfTIkJ6OaEtK0WfGzAYIM2PhE-hewCYIdxt37gq-9pSikqdV8kkMbJwFIB1rAE-5fOJFtjkb2DSE_BrQg8YI3UEUp-EQnA&oh=f1fdd3fbfc696d2f159da6ccc802a07d&oe=5B69D597'},
+  {title: 'dnfkjnd2', author: '', image:''}
 ];
 
 @Component({
